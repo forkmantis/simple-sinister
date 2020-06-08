8dec99ba3dcc82c5c9148b78e09b77dbd7b8970a
Will Killian <github.user@forkmantis.com>
2020-06-07 20:01:26 -0500

    Work Performed must be initialized with data and is immutable

--- simple/models/WorkPerformed.cs
+++ simple/models/WorkPerformed.cs
@@ -2,8 +2,15 @@ namespace simple.models
 {
     public class WorkPerformed
     {
-        public int Weight { get; set; }
-        public int Sets { get; set; }
-        public int Reps { get; set; }
+        public int Sets { get; private set; }
+        public int Reps { get; private set; }
+        public int Weight { get; private set; }
+
+        public WorkPerformed(int sets, int reps, int weight)
+        {
+            Sets = sets;
+            Reps = reps;
+            Weight = weight;
+        }
     }
 }
