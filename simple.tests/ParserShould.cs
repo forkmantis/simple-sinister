534d03eb85bc2215af000b683a65c89d82779fa3
Will Killian <github.user@forkmantis.com>
2020-06-11 06:22:19 -0500

    Parse optional notes
    Closes issue #1

--- simple.tests/ParserShould.cs
+++ simple.tests/ParserShould.cs
@@ -11,13 +11,30 @@ namespace simple.tests
         [InlineData("2020-05-03 wod: Pavel Timeless Simple LR 10x10 @ 45#;")]
         [InlineData("2020-05-03 wod: Pavel Timeless Simple LR 10 x 10 @ 45#;")]
         [InlineData("2020-05-03 wod: Pavel Timeless Simple LR 10x10@45#;")]
-        public void Parse_Valid_Single_Set(string input)
+        public void Parse_Valid_Single_Set_Without_Notes(string input)
         {
             // Arrange
             var parser = new Parser();
 
             var expected = new PracticeSession(DateTime.Parse("2020-05-03"), "Pavel Timeless Simple", "LR", String.Empty);
             expected.Swings.WorkPerformed.Add(new WorkPerformed(10, 10, 45));
+
+            // Act
+            var output = parser.Parse(input);
+
+            // Assert
+            output.Should().BeEquivalentTo(expected);
+        }
+
+        [Theory]
+        [InlineData("2020-05-04 wod: Pavel Timeless Simple T 10x10 @ 53#; notes: I didn't get much sleep the night before;")]
+        public void Parse_Valid_Single_Set_With_Notes(string input)
+        {
+            // Arrange
+            var parser = new Parser();
+
+            var expected = new PracticeSession(DateTime.Parse("2020-05-04"), "Pavel Timeless Simple", "T", "I didn't get much sleep the night before");
+            expected.Swings.WorkPerformed.Add(new WorkPerformed(10, 10, 53));
 
             // Act
             var output = parser.Parse(input);
--- simple/models/Parser.cs
+++ simple/models/Parser.cs
@@ -7,14 +7,14 @@ namespace simple.models
     {
         public PracticeSession Parse(string input)
         {
-            Regex outerRegex = new Regex(@"([0-9]{4}-[0-9]{2}-[0-9]{2}) *wod: *(Pavel.*) (LR|T) ([^;]+);? ?(notes:.*)?$");
+            Regex outerRegex = new Regex(@"([0-9]{4}-[0-9]{2}-[0-9]{2}) *wod: *(Pavel.*) (LR|T) ([^;]+);? ?(notes: *([^;]+);)?$");
             Match match = outerRegex.Match(input);
 
             var date = match.Groups[1].Value;
             var title = match.Groups[2].Value;
             var handedness = match.Groups[3].Value;
             var workingSets = match.Groups[4].Value;
-            var notes = match.Groups[5].Value;
+            var notes = match.Groups[6].Value;
 
             Regex innerRegex = new Regex(@" *([0-9]{1,2}) *x *([0-9]{1,2}) *@ *([0-9]{1,3})");
             Match innerMatch = innerRegex.Match(workingSets);
